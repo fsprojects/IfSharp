703c87025164f120526eae1893575cd046290adc
Peter Rosconi <peter@bayardrock.com>
2013-12-06 17:22:14 -0500

    Added displayPrinter functionality

--- src/IfSharp.Kernel/App.fs
+++ src/IfSharp.Kernel/App.fs
@@ -13,104 +13,120 @@ open fszmq.Socket
 
 module App = 
 
-    let Black        = "\u001B[0;30m"
-    let Blue         = "\u001B[0;34m"
-    let Green        = "\u001B[0;32m"
-    let Cyan         = "\u001B[0;36m"
-    let Red          = "\u001B[0;31m"
-    let Purple       = "\u001B[0;35m"
-    let Brown        = "\u001B[0;33m"
-    let Gray         = "\u001B[0;37m"
-    let DarkGray     = "\u001B[1;30m"
-    let LightBlue    = "\u001B[1;34m"
-    let LightGreen   = "\u001B[1;32m"
-    let LightCyan    = "\u001B[1;36m"
-    let LightRed     = "\u001B[1;31m"
-    let LightPurple  = "\u001B[1;35m"
-    let Yellow       = "\u001B[1;33m"
-    let White        = "\u001B[1;37m"
-    let Reset        = "\u001B[0m"
+    let internal Black        = "\u001B[0;30m"
+    let internal Blue         = "\u001B[0;34m"
+    let internal Green        = "\u001B[0;32m"
+    let internal Cyan         = "\u001B[0;36m"
+    let internal Red          = "\u001B[0;31m"
+    let internal Purple       = "\u001B[0;35m"
+    let internal Brown        = "\u001B[0;33m"
+    let internal Gray         = "\u001B[0;37m"
+    let internal DarkGray     = "\u001B[1;30m"
+    let internal LightBlue    = "\u001B[1;34m"
+    let internal LightGreen   = "\u001B[1;32m"
+    let internal LightCyan    = "\u001B[1;36m"
+    let internal LightRed     = "\u001B[1;31m"
+    let internal LightPurple  = "\u001B[1;35m"
+    let internal Yellow       = "\u001B[1;33m"
+    let internal White        = "\u001B[1;37m"
+    let internal Reset        = "\u001B[0m"
 
-    let mutable kernel:Option<IfSharpKernel> = None
+    let mutable internal kernel : Option<IfSharpKernel> = None
+    let mutable internal displayPrinters : list<Type * (obj -> BinaryOutput)> = []
 
     (** Convenience method for encoding a string within HTML *)
-    let htmlEncode(str) =
+    let internal htmlEncode(str) =
         System.Web.HttpUtility.HtmlEncode(str)
 
-    (** Displays a generic HTML string *)
-    let internal displayHtml (x:HtmlOutput) =
-        kernel.Value.SendDisplayData("text/html", x.Html)
+    (** Adds a custom display printer for extensibility *)
+    let internal addDisplayPrinter(printer : 'T -> BinaryOutput) =
+        displayPrinters <- (typeof<'T>, (fun (x:obj) -> printer (unbox x))) :: displayPrinters
 
-    (** Constructs an HTML table from the specified TableOutput *)
-    let internal displayTable(x:TableOutput) =
-        let sb = StringBuilder()
-        sb.Append("<table>") |> ignore
+    (** Default display printer *)
+    let internal defaultDisplayPrinter(x) =
+        { ContentType = "text/plain"; Data = sprintf "%A" x }
 
-        // output header
-        sb.Append("<thead>") |> ignore
-        sb.Append("<tr>") |> ignore
-        for col in x.Columns do
-            sb.Append("<th>") |> ignore
-            sb.Append(htmlEncode col) |> ignore
-            sb.Append("</th>") |> ignore
-        sb.Append("</tr>") |> ignore
-        sb.Append("</thead>") |> ignore
+    (** Finds a display printer based off of the type *)
+    let internal findDisplayPrinter(findType) = 
+        let printers = 
+            displayPrinters
+            |> Seq.filter (fun (t, _) -> t.IsAssignableFrom(findType))
+            |> Seq.toList
 
-        // output body
-        sb.Append("<tbody>") |> ignore
-        for row in x.Rows do
+        if printers.Length > 0 then
+            printers.Head
+        else
+            (typeof<obj>, defaultDisplayPrinter)
+
+    (** Adds default display printers *)
+    let internal addDefaultDisplayPrinters() = 
+        
+        // add generic chart printer
+        addDisplayPrinter(fun (x:ChartTypes.GenericChart) ->
+            { ContentType = "image/png"; Data = x.ToPng() }
+        )
+
+        // add chart printer
+        addDisplayPrinter(fun (x:GenericChartWithSize) ->
+            { ContentType = "image/png"; Data = x.Chart.ToPng(x.Size) }
+        )
+        
+        // add table printer
+        addDisplayPrinter(fun (x:TableOutput) -> 
+            let sb = StringBuilder()
+            sb.Append("<table>") |> ignore
+
+            // output header
+            sb.Append("<thead>") |> ignore
             sb.Append("<tr>") |> ignore
-            for cell in row do
-                sb.Append("<td>") |> ignore
-                sb.Append(htmlEncode cell) |> ignore
-                sb.Append("</td>") |> ignore
-                    
+            for col in x.Columns do
+                sb.Append("<th>") |> ignore
+                sb.Append(htmlEncode col) |> ignore
+                sb.Append("</th>") |> ignore
             sb.Append("</tr>") |> ignore
-        sb.Append("<tbody>") |> ignore
-        sb.Append("</tbody>") |> ignore
-        sb.Append("</table>") |> ignore
+            sb.Append("</thead>") |> ignore
 
-        displayHtml { Html = sb.ToString() }
+            // output body
+            sb.Append("<tbody>") |> ignore
+            for row in x.Rows do
+                sb.Append("<tr>") |> ignore
+                for cell in row do
+                    sb.Append("<td>") |> ignore
+                    sb.Append(htmlEncode cell) |> ignore
+                    sb.Append("</td>") |> ignore
+                    
+                sb.Append("</tr>") |> ignore
+            sb.Append("<tbody>") |> ignore
+            sb.Append("</tbody>") |> ignore
+            sb.Append("</table>") |> ignore
 
-    (** Displays a string *)
-    let displayString (x : string) =
-        kernel.Value.SendDisplayData("text/plain", x)
+            { ContentType = "text/html"; Data = sb.ToString() } 
+        )
 
-    (** Displays a generic object using sprintf "%A" *)
-    let displayGeneric (x : obj) =
-        displayString (sprintf "%A" x)
+        // add html printer
+        addDisplayPrinter(fun (x:HtmlOutput) ->
+            { ContentType = "text/html"; Data = x.Html }
+        )
+        
+        // add latex printer
+        addDisplayPrinter(fun (x:LatexOutput) ->
+            { ContentType = "text/latex"; Data = x.Latex }
+        )
 
-    (** Displays a generic chart by converting it to a png *)
-    let displayGenericChart (x : ChartTypes.GenericChart) =
-        kernel.Value.SendDisplayData("image/png", x.ToPng())
-
-    (** Displays a generic chart with size by converting it to a png with the size *)
-    let displayChartWithSize (x : GenericChartWithSize) =
-        kernel.Value.SendDisplayData("image/png", x.Chart.ToPng(x.Size))
-
-    (** Displays a generic chart with size by converting it to a png with the size *)
-    let displayLatex (x : LatexOutput) =
-        kernel.Value.SendDisplayData("text/latex", x.Latex)
-
-    (** Displays a generic object *)
-    let displayBinaryOutput (x : BinaryOutput) =
-        kernel.Value.SendDisplayData(x.ContentType, x.Data)
-
+    (** Global clear display function *)
     let Clear () = 
         kernel.Value.ClearDisplay()
 
     (** Global display function *)
     let Display (value : obj) =
-        match value with
-        | :? ChartTypes.GenericChart as chart -> displayGenericChart chart
-        | :? GenericChartWithSize    as chart -> displayChartWithSize chart
-        | :? TableOutput             as table -> displayTable table
-        | :? HtmlOutput              as html  -> displayHtml html
-        | :? LatexOutput             as math  -> displayLatex math
-        | :? BinaryOutput            as raw   -> displayBinaryOutput raw
-        | _                                   -> displayGeneric value
 
-    (** Displays help about an object *)
+        if value <> null then
+            let printer = findDisplayPrinter(value.GetType())
+            let (_, callback) = printer
+            let callbackValue = callback(value)
+            kernel.Value.SendDisplayData(callbackValue.ContentType, callbackValue.Data)
+
+    (** Global help function *)
     let Help (value : obj) = 
         let text = StringBuilder()
         let props = value.GetType().GetProperties() |> Seq.map (fun x -> x.Name) |> Seq.distinct |> Seq.toArray
@@ -137,6 +153,9 @@ module App =
 
     (** First argument must be an ipython connection file, blocks forever *)
     let Start (args : array<string>) = 
+
+        // adds the default display printers
+        addDefaultDisplayPrinters()
 
         // validate
         if args.Length < 1 then failwith "First argument must be a connection file for ipython"
