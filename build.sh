52a04ce66590bde4243e1bc50a2b83eeb4fbc280
Filippo Polo <filpolo@tin.it>
2016-05-06 13:54:41 +0100

    Merged with fsprojects jupyter

--- build.sh
+++ build.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
-  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion -Prerelease
+
+PREFIX="./"
+
+if test "$OS" = "Windows_NT"
+then
+  # use .Net
+  EXE=""
+  FLAGS=""
+else
+  EXE="mono"
+  FLAGS="-d:MONO"
 fi
-mono packages/FAKE/tools/FAKE.exe build.fsx $@
+
+if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
+    ${EXE} ${PREFIX}/.paket/paket.bootstrapper.exe
+    #${EXE} .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion -Prerelease
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        exit $exit_code
+    fi
+fi
+${EXE} ${PREFIX}/.paket/paket.exe restore
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+	exit $exit_code
+fi
+
+${EXE} ${PREFIX}/packages/FAKE/tools/FAKE.exe $@ --fsiargs ${FLAGS} build.fsx 
+
