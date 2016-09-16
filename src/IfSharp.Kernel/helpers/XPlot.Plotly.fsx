55c305ed3802380b9d83bf88b9fbf3e03343b02b
Sylvan Clebsch <syclebsc@microsoft.com>
2016-09-16 17:01:29 +0100

    Allow XPlot.Plotly charts to render as images
    Plotly has many lovely features, but large data sets rendered as
    JavaScript can be difficult to deal with. This adds Chart.Image,
    which renders a chart as a static PNG.

--- src/IfSharp.Kernel/helpers/XPlot.Plotly.fsx
+++ src/IfSharp.Kernel/helpers/XPlot.Plotly.fsx
@@ -14,3 +14,27 @@ do
 
     { Html = @"<script src=""https://cdn.plot.ly/plotly-latest.min.js""></script>" }
         |> Display
+
+type XPlot.Plotly.PlotlyChart with
+
+    member __.GetPngHtml() =
+        let html = __.GetInlineHtml()
+        html
+            .Replace("Plotly.newPlot(", "Plotly.plot(")
+            .Replace(
+                "data, layout);",
+                """data, layout)
+                .then(function(gd) {
+                    return Plotly.toImage(gd, {format: 'png'})
+                        .then(function(url) {
+                            gd.innerHTML = '<img src=' + url + ' />'
+                        });
+                });
+                """)
+
+type XPlot.Plotly.Chart with
+
+    static member Image (chart: PlotlyChart) =
+        { ContentType = "text/html"
+          Data = chart.GetPngHtml()
+        }
