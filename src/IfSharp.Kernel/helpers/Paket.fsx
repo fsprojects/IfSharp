b5dd27109efaddd89ce51bd803c33bbdf484f5fe
Sylvan Clebsch <sylvan.clebsch@gmail.com>
2016-09-26 09:56:47 +0100

    Add a Paket.fsx script (#86)
    * Add a Paket.fsx script
    
    This script loads `Paket.Core.dll` and sets up a `paket` object that
    is an instance of `Paket.Dependencies` that is initialised from
    `bin/paket.dependencies`. This allows simple use of Pajet from
    jupyter, like this:
    
    ```fsharp
    paket.GetDirectDependencies()
    ```
    
    * provide convenience syntax for adding deps
    
    * remove accidental nested module

---
+++ src/IfSharp.Kernel/helpers/Paket.fsx
@@ -0,0 +1,21 @@
+#nowarn "211"
+
+#r "Chessie.dll"
+#r "Paket.Core.dll"
+
+open System
+
+let private dir =
+    Reflection.Assembly.GetEntryAssembly().Location
+    |> IO.Path.GetDirectoryName
+
+Paket.Dependencies.Init(dir)
+let deps = Paket.Dependencies.Locate(dir)
+
+let Package list =
+    for package in list do
+        deps.Add(package)
+
+let Version list =
+    for package, version in list do
+        deps.Add(None, package, version)
