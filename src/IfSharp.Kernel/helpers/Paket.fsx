bd0606320bb09f1ccdb478b173eeb4d4e4c99226
Sylvan Clebsch <syclebsc@microsoft.com>
2016-09-26 09:56:07 +0100

    remove accidental nested module

--- src/IfSharp.Kernel/helpers/Paket.fsx
+++ src/IfSharp.Kernel/helpers/Paket.fsx
@@ -5,18 +5,17 @@
 
 open System
 
-module Paket =
-    let private dir =
-        Reflection.Assembly.GetEntryAssembly().Location
-        |> IO.Path.GetDirectoryName
+let private dir =
+    Reflection.Assembly.GetEntryAssembly().Location
+    |> IO.Path.GetDirectoryName
 
-    Paket.Dependencies.Init(dir)
-    let deps = Paket.Dependencies.Locate(dir)
+Paket.Dependencies.Init(dir)
+let deps = Paket.Dependencies.Locate(dir)
 
-    let Package list =
-        for package in list do
-           deps.Add(package)
+let Package list =
+    for package in list do
+        deps.Add(package)
 
-    let Version list =
-        for package, version in list do
-            deps.Add(None, package, version)
+let Version list =
+    for package, version in list do
+        deps.Add(None, package, version)
