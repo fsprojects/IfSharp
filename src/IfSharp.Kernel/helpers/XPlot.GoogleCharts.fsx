6a035cce50a8be62924653d74441869a68d75fb3
Colin Gravill <colin@gravill.com>
2017-03-17 13:50:05 +0000

    New API and prevent attempt to multiple load

--- src/IfSharp.Kernel/helpers/XPlot.GoogleCharts.fsx
+++ src/IfSharp.Kernel/helpers/XPlot.GoogleCharts.fsx
@@ -8,26 +8,25 @@ open IfSharp.Kernel
 open IfSharp.Kernel.App
 open IfSharp.Kernel.Globals
 
-@"<script src=""https://www.google.com/jsapi""></script>" |> Util.Html |> Display
-
 do
     use wc = new System.Net.WebClient()
     sprintf
         """
 <script type="text/javascript">
+if (google.charts.load == undefined) {
 %s
+}
 </script>
 """
         (wc.DownloadString("https://www.gstatic.com/charts/loader.js"))
         |> Util.Html
         |> Display
 
-
 type XPlot.GoogleCharts.GoogleChart with
   member __.GetContentHtml() =
     let html = __.GetInlineHtml()
     html
-      .Replace ("google.setOnLoadCallback(drawChart);", "google.load('current',{ packages: ['corechart'], callback: drawChart });")
+      .Replace ("google.setOnLoadCallback(drawChart);", "google.charts.load('current',{ packages: ['corechart'], callback: drawChart });")
 
 type XPlot.GoogleCharts.Chart with
   static member Content (chart : XPlot.GoogleCharts.GoogleChart) =
