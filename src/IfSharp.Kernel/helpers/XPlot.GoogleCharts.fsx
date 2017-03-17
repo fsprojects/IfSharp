01eebbe7c1c91a6377d4595ac6e40975716d4e47
Colin Gravill <cgravill@users.noreply.github.com>
2017-03-17 15:37:04 +0000

    Basic support for plotting with GoogleCharts (#139)
    * Initial XPlot.GoogleCharts support
    
    Adapted from #118
    The charts aren't present on a reload as the main script hasn't been
    loaded.
    
    * Safe load
    
    * Switch to downloading script
    
    This allows you to have charts when you reload the notebook
    
    * Ignore png/svg for now
    
    * Switch to current GC API
    
    https://developers.google.com/chart/interactive/docs/gallery/controls#load_your_libraries
    
    * New API and prevent attempt to multiple load
    
    * Include all chart types
    
    It makes for a larger download ~400KB rather than 300KB but allows the
    various chart types to work without modification.

---
+++ src/IfSharp.Kernel/helpers/XPlot.GoogleCharts.fsx
@@ -0,0 +1,36 @@
+#I "packages/XPlot.GoogleCharts/lib/net45/"
+#I "packages/Google.DataTable.Net.Wrapper/lib/"
+#r "XPlot.Googlecharts.dll"
+#r "Google.DataTable.Net.Wrapper.dll"
+#r "IfSharp.Kernel.dll"
+
+open IfSharp.Kernel
+open IfSharp.Kernel.App
+open IfSharp.Kernel.Globals
+
+do
+    use wc = new System.Net.WebClient()
+    sprintf
+        """
+<script type="text/javascript">
+if (loadedgooglecharts == undefined) {
+%s
+    var loadedgooglecharts = true;
+}
+</script>
+"""
+        (wc.DownloadString("https://www.gstatic.com/charts/loader.js"))
+        |> Util.Html
+        |> Display
+
+type XPlot.GoogleCharts.GoogleChart with
+  member __.GetContentHtml() =
+    let html = __.GetInlineHtml()
+    html
+      .Replace ("google.setOnLoadCallback(drawChart);", "google.charts.load('current',{ packages: ['corechart', 'annotationchart', 'calendar', 'gauge', 'geochart', 'map', 'sankey', 'table', 'timeline', 'treemap'], callback: drawChart });")
+
+type XPlot.GoogleCharts.Chart with
+  static member Content (chart : XPlot.GoogleCharts.GoogleChart) =
+    { ContentType = "text/html"; Data = chart.GetContentHtml() }
+
+AddDisplayPrinter (fun (plot: XPlot.GoogleCharts.GoogleChart) -> { ContentType = "text/html"; Data = plot.GetContentHtml() })
