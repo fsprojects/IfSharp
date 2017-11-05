ad6c99c5d827a0524f88b6e09375c362eb817f18
jancura <pavol.jancura@gmail.com>
2017-11-05 23:26:43 +0100

    Supporting Plotly and GoogleCharts for now

--- src/IfSharp.Kernel/helpers/FsLab.fsx
+++ src/IfSharp.Kernel/helpers/FsLab.fsx
@@ -4,21 +4,3 @@ Paket.Package [ "FsLab" ]
 
 #load "XPlot.GoogleCharts.fsx"
 #load "XPlot.Plotly.fsx"
-
-// Load the correct FSharp.Charting script
-open System
-open System.IO
-open System.Runtime.InteropServices
-
-let fsChartingScript = 
-    match RuntimeInformation.IsOSPlatform(OSPlatform.OSX) with
-    | true -> seq { yield "#load \"FSharp.Charting.Gtk.fsx\""  }
-    | _    -> seq { yield "#load \"FSharp.Charting.fsx\""  }
-
-let tempFile = Path.Combine(__SOURCE_DIRECTORY__, "__temp__.fsx")
-
-File.WriteAllLines(tempFile, fsChartingScript)    
-
-#load "__temp__.fsx"  
-
-File.Delete(tempFile)
