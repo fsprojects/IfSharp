c7a0226aff9fe6610420da71736fb29094edd483
Colin Gravill <colin.gravill@microsoft.com>
2018-06-13 14:19:12 +0100

    Move up the client dependencies to F# 4.3.4 and net461
    https://github.com/Microsoft/AzureNotebooks/issues/395

--- src/IfSharp.Kernel/helpers/Paket.fsx
+++ src/IfSharp.Kernel/helpers/Paket.fsx
@@ -60,5 +60,5 @@ let Version list =
 
 let Clear() =
     deps.GetInstalledPackages() |> List.iter (fun (_, package, _) -> remove_quiet package)
-    add "FSharp.Core" "= 4.2.1"
+    add "FSharp.Core" "= 4.3.4"
     ()
