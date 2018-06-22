c56a49cff0af2d2400db23367ad2978386b83e0b
Colin Gravill <colin.gravill@microsoft.com>
2018-06-22 15:49:22 +0100

    Note about stripping the Zone.Indentifier

--- src/IfSharp/Program.fs
+++ src/IfSharp/Program.fs
@@ -4,8 +4,8 @@ open System.Globalization
 open System.IO
 open Trinet.Core.IO.Ntfs
 
+//This hidden info can be leftover depending how you unzip a release on Windows
 let ClearAlternativeStreamsWindows() =
-
     let path = System.Reflection.Assembly.GetEntryAssembly().Location
     if path <> null then
         for filePath in (FileInfo(path).Directory.GetFileSystemInfos()) do
