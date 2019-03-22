fd2a805216ffac2e766e299185c159bfed0e98d0
rudihorn <rh@rudi-horn.de>
2019-03-22 12:41:20 +0100

    fixed incorrect assembly name references (#208)

--- src/IfSharp/AssemblyInfo.fs
+++ src/IfSharp/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace IFSharp.AssemblyInfo
+﻿namespace IfSharp.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("IFSharp")>]
+[<assembly: AssemblyTitle("IfSharp")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("IFSharp")>]
+[<assembly: AssemblyProduct("IfSharp")>]
 [<assembly: AssemblyCopyright("Copyright ©  2019")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
