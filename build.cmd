d861968d0237c3f2d61e322bf5d1ab50729d7a05
Colin Gravill <1099725+cgravill@users.noreply.github.com>
2019-10-12 15:25:36 +0100

    Fake with current .NET Core runner (#238)
    Moved to .NET Core based Fake, replacing the compatiblity mode we had
    Experimental .NET Core builds on macOS, Ubuntu, and Windows on Azure DevOps
    The Mono/.NET Framework worked for Windows but not Ubuntu on DevOps, so keeping Travis as well
    The Travis build was also running a Docker deploy, which on a branch doesn't totally make sense. I've disabled that for now, volunteers would be great.

--- build.cmd
+++ build.cmd
@@ -1,13 +1,1 @@
-@echo off
-cls
-
-.paket\paket.exe restore
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-IF NOT EXIST build.fsx (
-  .paket\paket.exe update
-  packages\FAKE\tools\FAKE.exe init.fsx
-)
-packages\FAKE\tools\FAKE.exe build.fsx %*
+fake.cmd build %*
