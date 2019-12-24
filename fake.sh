792d17a636d85bf3d73fd5b93075b8f9a8fe2585
Colin Gravill <1099725+cgravill@users.noreply.github.com>
2019-12-24 10:54:14 +0000

    Update Paket, FCS, and .NET Core (#244)
    * Update Paket, FCS, and .NET Core
    
    * Update pipeline for .NET Core 3.1
    
    * Try using .NET local tools
    https://stu.dev/dotnet-core-3-local-tools/
    
    * Tool restore for Travis as well
    
    * .NET Core 3.1.100 for Travis
    Just need >3.x for local tools

--- fake.sh
+++ fake.sh
@@ -3,28 +3,5 @@
 set -eu
 set -o pipefail
 
-# workaround macOS build agent issue from https://github.com/dotnet/cli/issues/9114#issuecomment-494226139
-export DOTNET_ROOT="$(dirname "$(readlink "$(command -v dotnet)")")"
-
-# liberated from https://stackoverflow.com/a/18443300/433393
-realpath() {
-  OURPWD=$PWD
-  cd "$(dirname "$1")"
-  LINK=$(readlink "$(basename "$1")")
-  while [ "$LINK" ]; do
-    cd "$(dirname "$LINK")"
-    LINK=$(readlink "$(basename "$1")")
-  done
-  REALPATH="$PWD/$(basename "$1")"
-  cd "$OURPWD"
-  echo "$REALPATH"
-}
-
-TOOL_PATH=$(realpath .fake)
-FAKE="$TOOL_PATH"/fake
-
-if ! [ -e "$FAKE" ]
-then
-  dotnet tool install fake-cli --tool-path "$TOOL_PATH"
-fi
-"$FAKE" "$@"
+dotnet tool restore
+dotnet fake "$@"
