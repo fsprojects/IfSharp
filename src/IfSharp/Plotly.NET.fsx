b2822c5fad944aa34cb2f05888679c024fd871f2
Helmut Dobretzberger <helmut.dobretzberger@active-group.de>
2022-01-27 15:45:49 +0100

    Add Plotly.NET as an available chart library

---
+++ src/IfSharp/Plotly.NET.fsx
@@ -0,0 +1,13 @@
+﻿#r "IfSharp.Kernel.dll"
+
+#load @".paket/load/main.group.fsx"
+
+open Plotly.NET
+open GenericChart
+open IfSharp.Kernel
+open IfSharp.Kernel.Globals
+
+do 
+    Printers.addDisplayPrinter(fun (plot: GenericChart.GenericChart) ->
+        { ContentType = "text/html"; Data = GenericChart.toChartHTML plot}
+    )
